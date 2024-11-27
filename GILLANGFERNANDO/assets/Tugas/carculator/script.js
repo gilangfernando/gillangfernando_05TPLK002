25c69c231270a16d63a5e461517466eef7ab5a7c
gilangfernando <gilangfernando480@gmail.com>
2024-11-27 16:24:53 +0700

    Add files via upload

---
+++ GILLANGFERNANDO/assets/Tugas/carculator/script.js
@@ -0,0 +1,17 @@
+function appendValue(value) {
+    document.getElementById("result").value += value;
+  }
+  
+  function clearResult() {
+    document.getElementById("result").value = "";
+  }
+  
+  function calculateResult() {
+    let result = document.getElementById("result").value;
+    try {
+      document.getElementById("result").value = eval(result);
+    } catch (e) {
+      document.getElementById("result").value = "Error";
+    }
+  }
+  
